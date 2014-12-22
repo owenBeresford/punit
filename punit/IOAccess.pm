8d4a0c0b30c7db6ae05d6ba618d032dffa339a7b
owen beresford <owenberesford@fastmail.fm>
2014-12-22 13:38:05 +0000

    Change method of detecting what methods to list, multiple times

--- punit/IOAccess.pm
+++ punit/IOAccess.pm
@@ -6,6 +6,11 @@ use 5.10.2;
 package punit::IOAccess;
 use Class::Inspector;
 use Try::Tiny ();
+use Exporter 'import';
+use version;
+use B qw( svref_2object );
+use Data::Dumper;
+
 use Exception::Class (
 		'BaseException',
 
@@ -14,6 +19,10 @@ use Exception::Class (
           description => 'Exception being unable to make valid PM file.'
       },
 );
+
+our @EXPORT = ();
+our @EXPORT_OK = qw( listAPI writeTestFile createTestPath );
+our $VERSION = '0.1.1';
 
 	sub new {
 		my ($caller, $priva)  		= @_;
@@ -30,11 +39,23 @@ use Exception::Class (
 	sub listAPI {
 		my ($self, $class) = @_;
 		my @out;
-		my $methods = Class::Inspector->methods( $class );
+#		my $methods = Class::Inspector->methods( $class );
+		my $methods = $self->_list_nonimported_subs($class); 
+
+
+#		my $parent = '';
+#		my $tmp = "\$parent = \$".$class."::ISA[ scalar(@".$class."::ISA) ] || undef(); ";
+#		eval($tmp);	
+#print "Whaqt is parent? ". Dumper $parent."\n";
 
 		for my $func (@$methods) {
 			print "listAPI: looking at '$func'." if($main::DEBUG);
-
+		
+#	print "can \$parent do $func? ".($parent->can($func)?"yes\n":"no\n");
+#  if($self->SUPER->can($func)) { print "method $func exists in ancestor.\n"; }
+# discard functions that are defined in the parent or higher classes.
+# doesn't cover situation where a subclass overrides a parents definition.
+#			next if ($parent && $parent->can($func));
 			next if ($func eq 'new');
 			next if (!$self->{private} && $func =~ m/^_/);
 
@@ -46,6 +67,27 @@ use Exception::Class (
 		else 			 { return \@out; }
 	}
 
+# http://stackoverflow.com/questions/12504744/perl-list-subs-in-a-package-excluding-imported-subs-from-other-packages
+	sub _list_nonimported_subs {
+		my ($self, $pkg_name) = @_;
+		my $pkg = do { no strict 'refs'; *{ $pkg_name . '::' } };
+
+		my @nonimported_subs=();
+		for my $name (keys %$pkg) {
+			my $glob = $pkg->{$name};
+			my $code = *$glob{CODE}
+			or next;
+
+			my $cv = svref_2object($code);
+			my $orig_pkg_name = $cv->GV->STASH->NAME;
+			next if $orig_pkg_name ne $pkg_name;
+
+			push @nonimported_subs, $name;
+		}
+
+		if(wantarray() ) { return @nonimported_subs; }
+		else 			 { return \@nonimported_subs; }
+	}
 
 	sub writeTestFile {
 		my ($self, $name, $data) = @_;
